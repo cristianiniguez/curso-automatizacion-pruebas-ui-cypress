5257fb696791fb054027cb43aff5474f293a25f0
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-10 19:04:52 -0400

    crea tu primera prueba: nuestra primera prueba

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+	e2e: {
+		excludeSpecPattern: [
+			'**/1-getting-started/*.js',
+			'**/2-advanced-examples/*.js',
+		],
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		viewportHeight: 1080,
+		viewportWidth: 1920,
+	},
+})
---
+++ cypress/e2e/first_test.cy.js
@@ -0,0 +1,5 @@
+describe('First test', () => {
+	it('Visit to our first page', () => {
+		cy.visit('https://www.platzi.com')
+	})
+})
